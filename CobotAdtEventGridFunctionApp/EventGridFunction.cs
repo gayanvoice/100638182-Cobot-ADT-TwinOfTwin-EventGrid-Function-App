eed47112935b632e379e7bb03be0be8cff125c9b
gayanvoice <gayan.kuruppu@live.com>
2023-08-05 09:39:28 +0100

    fix: error

--- CobotAdtEventGridFunctionApp/EventGridFunction.cs
+++ CobotAdtEventGridFunctionApp/EventGridFunction.cs
@@ -15,11 +15,11 @@ namespace CobotADTEventGridFunctionApp
     {
         private static readonly string adtInstanceUrl = Environment.GetEnvironmentVariable("ADT_SERVICE_URL");
 
-        [FunctionName("ProcessADTRoutedDataToTwinOfTwin")]
+        [FunctionName("ProcessADTRoutedDataOfTwinOfTwin")]
         public static async Task ProcessADTRoutedDataToTwinOfTwin([EventGridTrigger] EventGridEvent eventGridEvent, ILogger log)
         {
-            DefaultAzureCredential cred = new DefaultAzureCredential();
-            DigitalTwinsClient client = new DigitalTwinsClient(new Uri(adtInstanceUrl), cred);
+            DefaultAzureCredential defaultAzureCredential = new DefaultAzureCredential();
+            DigitalTwinsClient digitalTwinsClient = new DigitalTwinsClient(new Uri(adtInstanceUrl), defaultAzureCredential);
             log.LogInformation(eventGridEvent.Data.ToString());
             if (eventGridEvent != null && eventGridEvent.Data != null)
             {
@@ -30,18 +30,19 @@ namespace CobotADTEventGridFunctionApp
                 {
                     case "dtmi:com:Cobot:Cobot;1":
                         jsonPatchDocument.AppendReplace("/ElapsedTime", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/ElapsedTime")).Value);
-                        await client.UpdateDigitalTwinAsync("TCobot", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TCobot", jsonPatchDocument);
+                        log.LogInformation(JsonConvert.SerializeObject(jsonPatchDocument, Formatting.Indented));
                         break;
                     case "dtmi:com:Cobot:Payload;1":
                         jsonPatchDocument.AppendReplace("/Mass", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Mass")).Value);
                         jsonPatchDocument.AppendReplace("/CogX", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogX")).Value);
                         jsonPatchDocument.AppendReplace("/CogY", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogY")).Value);
                         jsonPatchDocument.AppendReplace("/CogZ", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/CogZ")).Value);
-                        await client.UpdateDigitalTwinAsync("TPayload", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TPayload", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:ControlBox;1":
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TControlBox", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TControlBox", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Elbow;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
@@ -50,37 +51,37 @@ namespace CobotADTEventGridFunctionApp
                         jsonPatchDocument.AppendReplace("/X", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/X")).Value);
                         jsonPatchDocument.AppendReplace("/Y", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Y")).Value);
                         jsonPatchDocument.AppendReplace("/Z", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Z")).Value);
-                        await client.UpdateDigitalTwinAsync("TElbow", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TElbow", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Base;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TBase", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TBase", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Shoulder;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TShoulder", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TShoulder", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Wrist1;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TWrist1", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TWrist1", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Wrist2;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TWrist2", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TWrist2", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Wrist3;1":
                         jsonPatchDocument.AppendReplace("/Position", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Position")).Value);
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
                         jsonPatchDocument.AppendReplace("/Voltage", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Voltage")).Value);
-                        await client.UpdateDigitalTwinAsync("TWrist3", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TWrist3", jsonPatchDocument);
                         break;
                     case "dtmi:com:Cobot:JointLoad:Tool;1":
                         jsonPatchDocument.AppendReplace("/Temperature", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Temperature")).Value);
@@ -91,7 +92,7 @@ namespace CobotADTEventGridFunctionApp
                         jsonPatchDocument.AppendReplace("/Rx", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rx")).Value);
                         jsonPatchDocument.AppendReplace("/Ry", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Ry")).Value);
                         jsonPatchDocument.AppendReplace("/Rz", rootObject.Data.Patch.Find(patch => patch.Path.Contains("/Rz")).Value);
-                        await client.UpdateDigitalTwinAsync("TTool", jsonPatchDocument);
+                        await digitalTwinsClient.UpdateDigitalTwinAsync("TTool", jsonPatchDocument);
                         break;
                     default:
                         break;
